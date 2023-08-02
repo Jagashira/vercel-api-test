b6abd670224aef03dc20fe5b2c375230671d990a
Jagashira <pyprogjas.3104@gmail.com>
2023-08-02 12:37:11 +0900

    tmp

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,53 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+// export {}
+const { chromium } = require('playwright');
 
-type Data = {
-  name: string
-}
+(async () => {
+  const browser = await chromium.launch({
+    headless: true
+  });
+  const context = await browser.newContext();
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+  // Open new page
+  const page = await context.newPage();
+
+  // Go to https://qiita.com/
+  await page.goto('https://qiita.com/');
+
+  // Click input[name="q"]
+  await page.click('input[name="q"]');
+
+  // Fill input[name="q"]
+  await page.fill('input[name="q"]', 'playwright-cli');
+
+  // Press Enter
+  await page.press('input[name="q"]', 'Enter');
+  // assert.equal(page.url(), 'https://qiita.com/search?q=playwright-cli');
+
+  // Click text=/.*関連順.*/
+  await page.click('text=/.*関連順.*/');
+
+  // Click text=/.*新着順.*/
+  await page.click('text=/.*新着順.*/');
+  // assert.equal(page.url(), 'https://qiita.com/search?q=playwright-cli&sort=created');
+
+  // Click div[id="main"] input[name="q"]
+  await page.click('div[id="main"] input[name="q"]');
+
+  // Fill div[id="main"] input[name="q"]
+  await page.fill('div[id="main"] input[name="q"]', 'playwright-cli video');
+
+  // Click text=/.*検索.*/
+  await page.click('text=/.*検索.*/');
+  // assert.equal(page.url(), 'https://qiita.com/search?sort=created&q=playwright-cli+video');
+
+  // Click //*[local-name()="svg" and normalize-space(.)='Qiita']
+  await page.click('//*[local-name()="svg" and normalize-space(.)=\'Qiita\']');
+  // assert.equal(page.url(), 'https://qiita.com/');
+
+  // Close page
+  await page.close();
+
+  // ---------------------
+  await context.close();
+  await browser.close();
+})();
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react"
 
 
 export default function Home() {
-  const avator = { "face": "rounded", "name": "sara" }
+
+  const [users, setUsers] = useState<Array>("");
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch("api/apitest");
+      const data = await response.json();
+      setUsers(data.users);
+      console.log(data.users)
+    }
+    fetchUsers()
+  }, [])
   return (
     <>
+      <div>
+        <ul>
+          {users.map((user) => {
+            (<li key={user.id}>{user.name}</li>)
+          })}</ul>
+
+
+      </div>
 
 
     </>
